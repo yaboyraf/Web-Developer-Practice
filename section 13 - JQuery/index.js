9936352c8300957cb03f275f47963b6e05218d22
Rafael Guevara <mmmsnacks@gmail.com>
2019-01-20 15:57:57 -0800

    animations!

--- section 13 - JQuery/index.js
+++ section 13 - JQuery/index.js
@@ -8,8 +8,8 @@
 //     });
 // }
 
-$('button').click(function() {
-    $('h1').css('color', 'purple');
+$('button').on('click', function() {
+    $('h1').slideUp().slideDown().animate({opacity: 0.5});
 });
 
 $(document).keypress(function(event) {
