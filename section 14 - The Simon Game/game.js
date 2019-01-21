6c07b891195caeda071df540ef963eebbe485632
Rafael Guevara <mmmsnacks@gmail.com>
2019-01-21 15:44:13 -0800

    added animations

--- section 14 - The Simon Game/game.js
+++ section 14 - The Simon Game/game.js
@@ -1,22 +1,36 @@
-//3. make an array containing colors
+//2.3. make an array containing colors
 const buttonColors = ['red', 'blue', 'green', 'yellow'];
 
-//5. create an empty array called gamePattern
+//2.5. create an empty array called gamePattern
 const gamePattern = [];
 
-//1. make a function called nextSequence
-function nextSequence() {
-    //2. generate a number between 0 and 3
-    const randomNumber = Math.floor(Math.random() * 4);
-    //console.log(randomNumber);
+//4.3. create a new empty array called userClickedPattern
+const userClickedPattern = [];
 
-    //4. create a variable called randomChosenColor and use randomNumber to select a random color from buttonColors array
+//2.1. make a function called nextSequence
+function nextSequence() {
+    //2.2. generate a number between 0 and 3
+    const randomNumber = Math.floor(Math.random() * 4);
+
+    //2.4. create a variable called randomChosenColor and use randomNumber to select a random color from buttonColors array
     const randomChosenColor = buttonColors[randomNumber];
 
-    //6. add randomChosenColor from step 4 to the end of gamePattern;
+    //2.6. add randomChosenColor from step 4 to the end of gamePattern;
     gamePattern.push(randomChosenColor);
     console.log('the color is: ' + randomChosenColor);
 
+    //3.1. use JQuery to select the button with the same id as randomChosenColor
+    $('#' + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100); //3.2. flash animation
+
+    //3.3. playSound
+    const audio = new Audio('sounds/' + randomChosenColor + '.mp3');
+    audio.play();
+
 };
 
-nextSequence();
+//4.1. use JQuery to create a handler function to detect clicks
+$('.btn').click(function() {
+    //4.2 
+    const userChosenColor = this.attr('id');
+
+});
