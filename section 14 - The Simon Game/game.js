fd9a4868ee21e2c0694a2d3b96e2a96dded93243
Rafael Guevara <mmmsnacks@gmail.com>
2019-01-23 16:21:29 -0800

    added button animations

--- section 14 - The Simon Game/game.js
+++ section 14 - The Simon Game/game.js
@@ -7,6 +7,20 @@ const gamePattern = [];
 //4.3. create a new empty array called userClickedPattern
 const userClickedPattern = [];
 
+//4.1. use JQuery to create a handler function to detect clicks
+$('.btn').click(function() {
+    //4.2 
+    const userChosenColor = $(this).attr('id');
+    //4.4. add contents of the variable userChosenColor to the end of the new userClickedPattern
+    userClickedPattern.push(userChosenColor);
+    //5.1
+    playSound(userChosenColor);
+
+    animatePress(userChosenColor);
+
+    //console.log('the pattern is: ' + userClickedPattern);
+});
+
 //2.1. make a function called nextSequence
 function nextSequence() {
     //2.2. generate a number between 0 and 3
@@ -21,19 +35,21 @@ function nextSequence() {
 
     //3.1. use JQuery to select the button with the same id as randomChosenColor
     $('#' + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100); //3.2. flash animation
-
-    //3.3. playSound
-    const audio = new Audio('sounds/' + randomChosenColor + '.mp3');
-    audio.play();
-
+    
+    //5.4. when user clicks a color, sound will play
+    playSound(randomChosenColor);
 };
 
-//4.1. use JQuery to create a handler function to detect clicks
-$('.btn').click(function() {
-    //4.2 
-    const userChosenColor = $(this).attr('id');
-    //4.4. add contents of the variable userChosenColor to the end of the new userClickedPattern
-    userClickedPattern.push(userChosenColor);
+//5.2. create a new sound function called playSound that takes in (name)
+function playSound(name) {
+    //5.3. move the Audio() function from nextSequence
+    const audio = new Audio('sounds/' + name + '.mp3');
+    audio.play();
+}
 
-    console.log('the pattern is: ' + userClickedPattern);
-});
+function animatePress(currentColor) {
+    $('#' + currentColor).addClass('pressed');
+    setTimeout(function() {
+        $('#' + currentColor).removeClass('pressed');
+    }, 10);
+}
