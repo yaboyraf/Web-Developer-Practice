c57e4babe7c2ab17c3675bdea71a03c32744cc4c
Rafael Guevara <mmmsnacks@gmail.com>
2019-01-27 18:56:12 -0800

    practice

---
+++ strings_practice/variables.js
@@ -0,0 +1,15 @@
+//1.you can't define a variable more than once
+let petName = 'Truffles'
+petName = 'Lily'
+
+console.log(petName)
+
+//2. there are rules related to variable names
+//variables have to start with a letter, $, or _
+//let 3 = 2 this won't work
+let $name = 3
+
+console.log($name)
+
+//3. variables names cannot be reserved keywords
+// let let = 12 this won't work
