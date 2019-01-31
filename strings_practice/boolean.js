6a65342a11b0c3003eb2eb15889243c211751224
Rafael Guevara <mmmsnacks@gmail.com>
2019-01-30 17:56:13 -0800

    if statements

--- strings_practice/boolean.js
+++ strings_practice/boolean.js
@@ -1,3 +1,29 @@
-let isFreezing = false
+// === - equality operator
+// !== - not equal operator
+// < - less than operator
+// > - greater than operator
+// <= less than or equal to operator
+// >= greater than or equal to operator
 
-console.log(isFreezing)
+// let temp = 100
+
+// if (temp <= 32) {
+//     console.log(`it's cold outside`)
+// }
+
+// if (temp >= 110) {
+//     console.log(`it's hot outside`)
+// }
+
+let age = 35
+
+// let isChild = age <= 7
+// let isSenior = age >= 65
+
+if(age <= 7) {
+    console.log('you are eligible for child pricing!')
+} else if(age >= 65) {
+    console.log('you are eligible for a senior discount!')
+} else {
+    console.log('you will have to play full price :(')
+}
