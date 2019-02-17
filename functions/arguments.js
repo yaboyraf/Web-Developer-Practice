462e4f954be50238e57482bb83a964ce48194535
Rafael Guevara <mmmsnacks@gmail.com>
2019-02-16 18:25:54 -0800

    arguments/template strings

--- functions/arguments.js
+++ functions/arguments.js
@@ -8,7 +8,7 @@ console.log(result)
 
 //default arguments 
 let getScoreText = function (name = 'Anonymous', score = 0) {
-    return 'Name: ' + name + ', Score: ' + score
+    return `Name: ' ${name}, Score: ${score}`
 }
 
 let scoreText = getScoreText(undefined, 100)
@@ -16,10 +16,13 @@ console.log(scoreText)
 
 //challenge
 //total, tipPercent .2
-
+//A 25% tip on $40 would be $10
 let getTip = function (total, percentage = .2) {
-    return total * percentage
+    let percent = percentage * 100
+    let tip = total * percentage
+    return `A ${percent}% tip on $${total} would be $${tip}`
 }
 
-let totalWithTip = getTip(56.29, .30)
+let totalWithTip = getTip(60)
 console.log(totalWithTip)
+
