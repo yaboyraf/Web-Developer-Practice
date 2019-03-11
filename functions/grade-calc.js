321647e647aee180d9f233a5370c24bc339fb305
Rafael Guevara <mmmsnacks@gmail.com>
2019-03-11 08:22:55 -0700

    objects

---
+++ functions/grade-calc.js
@@ -0,0 +1,26 @@
+//students score, total possible score
+//15/20 -> you got a C (75%)
+//A 90-100, B 80-89, C 70-79, D 60-69, F 0-59
+
+let gradeCalculator = function(score, total) {
+    let percentScore = (score / total) * 100;
+    let letterGrade = ''
+
+    if (percentScore >= 90) {
+        letterGrade = 'A'
+    } else if (percentScore >= 80) {
+        letterGrade = 'B'
+    } else if (percentScore >= 70) {
+        letterGrade = 'C'
+    } else if (percentScore >= 60) {
+        letterGrade = 'D'
+    } else {
+        letterGrade = 'F'
+    }
+
+    return `With a score of ${score}/${total}(${percentScore}%), your letter grade is: ${letterGrade}`
+}
+
+
+let result = gradeCalculator(19, 20)
+console.log(result)
---
+++ objects/objects-101.js
@@ -0,0 +1,25 @@
+let myBook = {
+    title: '1984',
+    author: 'George Orwell',
+    pageCount: 326
+}
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+myBook.title = 'Animal Farm'
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+// challenge
+
+let me = {
+    name: 'Raf',
+    age: 25,
+    location: 'San Jose'
+}
+
+console.log(`${me.name} is ${me.age} and lives in ${me.location}`)
+
+me.age = me.age + 1
+
+console.log(`${me.name} is ${me.age} and lives in ${me.location}`)
