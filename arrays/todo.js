3a43d756b8aded2914964ddd79387e25cf283ab5
Rafael Guevara <mmmsnacks@gmail.com>
2019-03-18 09:40:38 -0700

    arrays

---
+++ arrays/todo.js
@@ -0,0 +1,5 @@
+const todos = ['Get dressed', 'go to work', 'do work', 'go home']
+
+console.log(`You have ${todos.length} todos on your list`)
+console.log(`Todo: ${todos[0]}`)
+console.log(`Todo: ${todos[todos.length - 2]}`)
