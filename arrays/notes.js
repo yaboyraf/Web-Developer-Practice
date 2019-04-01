1aca18be9546e5ac6c253e09aa3d1bac2a4d115c
Rafael Guevara <mmmsnacks@gmail.com>
2019-04-01 12:39:26 -0700

    for loops

--- arrays/notes.js
+++ arrays/notes.js
@@ -8,11 +8,14 @@ const notes = ['Note 1', 'Note 2', 'Note 3']
 
 //notes.splice(1, 0, 'this is the new second item')
 
+//CALLBACK FUNCTION = a function passed into a function
+
 notes[2] = 'This is now the new note 3'
 
 
-notes.forEach(function() {
-    console.log('testing123')
+notes.forEach(function(item, index) {
+    console.log(index)
+    console.log(item)
 })
 
 console.log(notes.length)
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,12 +1,19 @@
-const todos = ['Get dressed', 'go to work', 'do work', 'go home']
+const todos = ['Wake up', 'Get dressed', 'go to work', 'do work', 'go home']
 
 // delete the third item
 // add a new item in the end
 // remove the first from the list
 
 todos.splice(2, 1)
-todos.push('go to sleep')
-todos.shift(0)
+todos.push('get coffee')
+todos.shift()
 
 console.log(`You have ${todos.length} todos on your list`)
-console.log(todos)
+
+//1. the first item
+//2. the second item
+
+todos.forEach(function(todo, index) {
+    const num = index + 1
+    console.log(`${num}. ${todo}`)
+})
