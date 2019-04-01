e2ed7e2d2a85cb90bf95f24ca88866cb83133d21
Rafael Guevara <mmmsnacks@gmail.com>
2019-04-01 12:21:27 -0700

    foreach

--- arrays/notes.js
+++ arrays/notes.js
@@ -10,5 +10,10 @@ const notes = ['Note 1', 'Note 2', 'Note 3']
 
 notes[2] = 'This is now the new note 3'
 
+
+notes.forEach(function() {
+    console.log('testing123')
+})
+
 console.log(notes.length)
 console.log(notes)
