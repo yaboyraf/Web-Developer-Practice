3c717274d73442246833416c13b234e67167fb48
Rafael Guevara <mmmsnacks@gmail.com>
2019-04-04 19:02:57 -0700

    DOM stuff

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,15 @@
-console.log('hi i am working!')
+// the DOM - Document Object Model
+
+// Query and remove
+// const p = document.querySelector('p')
+// p.remove()
+
+// Query all and remove
+
+const ps = document.querySelectorAll('p')
+
+ps.forEach(function(p) {
+    p.textContent = 'foo'
+    //console.log(p.textContent)
+    // p remove
+})
