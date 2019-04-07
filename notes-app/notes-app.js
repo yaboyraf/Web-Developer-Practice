3d216c4cc712f05e07fb583f099c5fce86b3c9fd
Rafael Guevara <mmmsnacks@gmail.com>
2019-04-06 21:24:30 -0700

    SEARCH

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -9,6 +9,26 @@ const notes = [{
     body: 'Better cable management'
 }]
 
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = function(notes, filters) {
+    const filteredNotes = notes.filter(function(note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = ''
+
+    filteredNotes.forEach(function(note) {
+        const noteElement = document.createElement('p')
+        noteElement.textContent = note.title
+        document.querySelector('#notes').appendChild(noteElement)
+    })
+}
+
+renderNotes(notes, filters)
+
 document.querySelector('#add-btn').addEventListener('click', function(e) {
     e.target.textContent = 'Notes Added!'
 })
@@ -20,5 +40,7 @@ document.querySelector('#remove-all').addEventListener('click', function() {
 })
 
 document.querySelector('#search-text').addEventListener('input', function(e) {
-    console.log(e.target.value)
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
 })
+
