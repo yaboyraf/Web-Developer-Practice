297e34b039a1596f0079b3b247c453d01168171b
Rafael Guevara <mmmsnacks@gmail.com>
2019-04-08 09:30:47 -0700

    todo app search

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -15,20 +15,34 @@ const todos = [{
     completed: false
 }]
 
-//looks for incomplete todos
-const incompleteTodos = todos.filter(function(todo) {
-    return !todo.completed
-})
 
-const summary = document.createElement('h3')
-summary.textContent = `You have ${incompleteTodos.length} todos left`
-document.querySelector('body').appendChild(summary)
+const filters = {
+    searchText: ''
+}
 
-todos.forEach(function(todo) {
-    const p = document.createElement('p')
-    p.textContent = todo.text
-    document.querySelector('body').appendChild(p)
-})
+const renderTodos = function(todos, filters) {
+    const filteredTodos = todos.filter(function(todo) {
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    const incompleteTodos = filteredTodos.filter(function(todo) {
+        return !todo.completed
+    })
+
+    document.querySelector('#todos').innerHTML = ''
+    
+    const summary = document.createElement('h3')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    document.querySelector('#todos').appendChild(summary)
+    
+    filteredTodos.forEach(function(todo) {
+        const p = document.createElement('p')
+        p.textContent = todo.text
+        document.querySelector('#todos').appendChild(p)
+    })
+}
+
+renderTodos(todos, filters)
 
 document.querySelector('#input-1').addEventListener('input', function(e) {
     console.log(e.target.values)
@@ -38,3 +52,9 @@ document.querySelector('#input-1').addEventListener('input', function(e) {
 document.querySelector('#add-btn').addEventListener('click', function(ev) {
     ev.target.textContent = 'Todo added!'
 })
+
+document.querySelector('#search-txt').addEventListener('input', function(e){
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
+})
+
