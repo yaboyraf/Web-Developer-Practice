04a5d731c7b95c9f18db1c4bbbac508e897d330c
Rafael Guevara <mmmsnacks@gmail.com>
2019-11-29 20:51:29 -0800

    relearning strings and variables

---
+++ strings.js
@@ -0,0 +1,12 @@
+// let firstName = 'Raf'
+// let lastName = 'Guevara'
+// let fullName = firstName + ' ' + lastName
+
+// console.log(`${firstName} ${lastName}`)
+// console.log(fullName)
+
+let city = 'San Jose'
+let country = 'United States'
+let location = city + ', ' + country
+
+console.log(location)
