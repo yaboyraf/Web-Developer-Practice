ef49a82f5b7a2c4c37f6a25f925d0967c9818c51
Rafael Guevara <mmmsnacks@gmail.com>
2019-12-03 19:31:03 -0800

    mini challenge from boolean video

--- boolean.js
+++ boolean.js
@@ -10,4 +10,17 @@ let isFreezing = temp <= 33
 
 console.log(isFreezing)
 
+// challenge area
 let age = 26 
+
+let isChild = age <= 7
+let isSenior = age >= 65
+
+console.log(isChild)
+console.log(isSenior)
+
+// create age set to your age
+// calculate is child - if THEY ARE 7 OR UNDER
+// calculate is senior - if they are 65 or older
+// print is child value
+// print is senior value
