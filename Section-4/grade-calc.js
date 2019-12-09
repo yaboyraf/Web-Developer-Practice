79a194fa0250b41ac7273f5e43eb321cb5c0d5b3
Rafael Guevara <mmmsnacks@gmail.com>
2019-12-08 16:46:44 -0800

    grade calculator project

--- Section-4/grade-calc.js
+++ Section-4/grade-calc.js
@@ -2,3 +2,19 @@
 // 15/20 -> you got a C (75%)
 // A 90-100, B 80-89, C 70-79, D 60-69, F 0-59
 
+// undefined for function arguments 
+// undefined as function return default value
+let gradeCalc = function (num) {
+    console.log(num)
+}
+
+let result = square()
+
+console.log(result)
+
+// null as assigned values
+let age = 27
+
+age = null
+
+console.log(age)
