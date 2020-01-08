851dcdf55b7742cbea36ce4a7010b6187c5e1397
Rafael Guevara <mmmsnacks@gmail.com>
2020-01-07 20:48:07 -0800

    basic array stuff

---
+++ Section-6/notes.js
@@ -0,0 +1,14 @@
+const notes = ['Note 1', 'Note 2', 'Note 3']
+
+// console.log(notes.pop())
+// notes.push('New note')
+
+// //console.log(notes.shift())
+// console.log(notes.unshift('My first note'))
+
+//notes.splice(1, 1, 'This is the new second item')
+
+notes[2] = 'This is now the new note 3'
+
+console.log(notes.length)
+console.log(notes)
